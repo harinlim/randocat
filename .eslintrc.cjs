928db7fe303496c84a2171ac5a927b20dbc11364
Harin Lim <hlim@unc.edu>
2024-06-09 17:59:31 -0400

    feat: initial commit (`create vite` with ts + swc)

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+// @ts-check
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked', // @typescript-eslint @v6
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest', // Infer typing on specified ECMAScript features - should be in parity with TSConfig lib
+    ecmaFeatures: {
+      jsx: true, // Enable JSX parsing
+    },
+    project: './tsconfig.json', // Configure parser to use TSConfig specified for type information
+    sourceType: 'module', // Use ESM source for imports
+  },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
---
+++ .prettierrc.cjs
@@ -0,0 +1,15 @@
+// @ts-check
+/** @type {import('prettier').Config} */
+module.exports = {
+  semi: false,
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  endOfLine: 'lf',
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  plugins: [],
+}
